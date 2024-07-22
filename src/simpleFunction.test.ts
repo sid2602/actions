a0bb79d0d08857caa584c16025adc693a5b0c1ac
sid2602 <sidhd2602@gmail.com>
2024-07-22 16:59:12 +0200

    add healthcheck workflow

--- src/simpleFunction.test.ts
+++ src/simpleFunction.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "vitest";
 import { simpleFunction } from "./simpleFunction";
 
 test('should return "Only for testing" string', () => {
-	expect(simpleFunction()).toBe("Only for testing");
+	expect(simpleFunction()).toBe("Only for testin");
 });
